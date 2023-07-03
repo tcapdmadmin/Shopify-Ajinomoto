abda8d45b78d5d0b28ac3e9e8c090bb73578d297
tcapjean <133844623+tcapjean@users.noreply.github.com>
2023-07-03 15:40:15 +0800

    Fix Mobile login, remove debug console logs

--- assets/custcomcode-login.js
+++ assets/custcomcode-login.js
@@ -2,7 +2,7 @@
 const apiUrl = "https://ajith-api.tcapdm.com";
 var base_url = window.location.origin;
 let is_en = window.location.pathname;
-let selectedGroup = '';
+let selectedGroup = "";
 const segments = is_en.split("/");
 // Check if "en" exists in the first segment (index 1) of the string
 if (segments[1] === "en") {
@@ -11,14 +11,22 @@ if (segments[1] === "en") {
   is_en = "";
 }
 document.addEventListener("DOMContentLoaded", () => {
-
   function handleSignInToLine() {
-
-    const swal_shop_type_select_doc = document.getElementById("swal_shop_type_select").innerHTML;
-    const swal_shop_type_description_doc = document.getElementById("swal_shop_type_description").innerHTML;
-    const swal_shop_type_confirm_doc = document.getElementById("swal_shop_type_confirm").innerHTML;
-    const swal_shop_type_deny_doc = document.getElementById("swal_shop_type_deny").innerHTML;
-    const swal_shop_type_cancel_doc = document.getElementById("swal_shop_type_cancel").innerHTML;
+    const swal_shop_type_select_doc = document.getElementById(
+      "swal_shop_type_select"
+    ).innerHTML;
+    const swal_shop_type_description_doc = document.getElementById(
+      "swal_shop_type_description"
+    ).innerHTML;
+    const swal_shop_type_confirm_doc = document.getElementById(
+      "swal_shop_type_confirm"
+    ).innerHTML;
+    const swal_shop_type_deny_doc = document.getElementById(
+      "swal_shop_type_deny"
+    ).innerHTML;
+    const swal_shop_type_cancel_doc = document.getElementById(
+      "swal_shop_type_cancel"
+    ).innerHTML;
 
     Swal.fire({
       title: swal_shop_type_select_doc,
@@ -58,98 +66,102 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   const signupLineBtn = document.getElementById("lineConnectBtn");
   signupLineBtn.addEventListener("click", handleSignInToLine);
-
 });
-  // Set Line Login URL
-  const setLineLoginURLa = (param = "") => {
-    return fetch(
-      "https://ajith-api.tcapdm.com/api/line/login/getlink?mode=login&shop_group=" +
-        param,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          mode: "login",
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
+// Set Line Login URL
+const setLineLoginURLa = (param = "") => {
+  return fetch(
+    "https://ajith-api.tcapdm.com/api/line/login/getlink?mode=login&shop_group=" +
+      param,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        mode: "login",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("API request failed");
       }
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("API request failed");
-        }
-        return response.json();
-      })
-      .then((data) => {
-
-        if (data.results) {
-          return data;
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        alert("Failed to get login URL");
-      });
-  };
-
+      return response.json();
+    })
+    .then((data) => {
+      if (data.results) {
+        return data;
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Failed to get login URL");
+    });
+};
 
 function autoFillLogin() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const code = urlParams.get("code");
-  const shopType = retrieveShopType();
 
   if (code) {
-    const apiUrl = "https://ajith-api.tcapdm.com/api/login/line/verify?shop_group=" +
-    shopType;
-    const payload = { code: code };
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    };
-
-    fetch(apiUrl, options)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.code == 200) {
-          storeShopType('');
-          let CustomerEmail = document.getElementById("CustomerEmail");
-          CustomerEmail.value = data.results.email;
-          let CustomerPassword = document.getElementById("CustomerPassword");
-          CustomerPassword.value = data.results.password;
-          setTimeout(() => {
-            const loginSubmit = document.getElementById("loginSubmit");
-            loginSubmit.click();
-          }, 2000);
-        } else {
-          let message = "Unexpected error occurred";
-          if (data.message) {
-            message = data.message;
-          }
-          Swal.fire({
-            icon: "error",
-            title: "Error",
-            text: message,
-          }).then(() => {
-            window.location.href = base_url + is_en + "/account/login";
-          });
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-      });
+    setTimeout(autoFillExecute(code), 3500);
   }
+}
+
+function autoFillExecute(code) {
+  const shopType = retrieveShopType();
+  const apiUrl =
+    "https://ajith-api.tcapdm.com/api/login/line/verify?shop_group=" + shopType;
+  const payload = { code: code };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(apiUrl, options)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.code == 200) {
+        Swal.showLoading();
+
+        storeShopType("");
+        let CustomerEmail = document.getElementById("CustomerEmail");
+        CustomerEmail.value = data.results.email;
+        let CustomerPassword = document.getElementById("CustomerPassword");
+        CustomerPassword.value = data.results.password;
+        setTimeout(() => {
+          localStorage.clear();
+          const loginSubmit = document.getElementById("loginSubmit");
+          loginSubmit.click();
+        }, 2000);
+      } else {
+        let message = "Unexpected error occurred";
+        if (data.message) {
+          message = data.message;
+        }
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: message,
+        }).then(() => {
+          window.location.href = base_url + is_en + "/account/login";
+        });
+      }
+    })
+    .catch((error) => {
+      localStorage.clear();
+      console.log(error);
+    });
 }
 autoFillLogin();
 
-
 function storeShopType(shopType) {
-  sessionStorage.setItem("shopType", shopType);
+  localStorage.setItem("shopType", shopType);
 }
 function retrieveShopType() {
-  return sessionStorage.getItem("shopType");
+  return localStorage.getItem("shopType");
 }
